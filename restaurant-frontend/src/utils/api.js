5e40afae4dfed30a86b23dc8afd1656f634afb4b
Durga <yadlapallisivanagadurga@gmail.com>
2026-03-04 15:37:51 +0530

    port added in appjs

--- restaurant-frontend/src/utils/api.js
+++ restaurant-frontend/src/utils/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://food-delivery-app-z30l.onrender.com",
-  headers: {
-    "Cache-Control": "no-cache",
-    Pragma: "no-cache",
-    Expires: "0",
-  },
+  baseURL: "http://34.230.5.7:8080"
 });
 
 export default api;
